6b9ac77394c6245a1a2455f8ffa6dec56c242122
Aaron <85681241+27Aaron@users.noreply.github.com>
2026-06-23 18:10:39 +0800

    Fix bat rose pine theme source

--- modules/core/programs/bat.nix
+++ modules/core/programs/bat.nix
@@ -6,15 +6,15 @@
         src = pkgs.fetchFromGitHub {
           owner = "drluckyspin";
           repo = "rose-pine-bat";
-          rev = "c0bfb1fda311159397031cbf07411a6349b49e32";
-          sha256 = "sha256-p0AR47OtBcQyyGJOjf+EjRQw0ckyUhcdeRqb7sA0zLI=";
+          rev = "5f10349e9ff32cd60d28debc3fa71fe6f5470c34";
+          sha256 = "sha256-FpUgVQ7vx9iY7TkPsu2cMRQyaY8ZDswVHVpSNqXRtfU=";
         };
         file = "themes/Rose-Pine.tmTheme";
       };
     };
     config = {
-      pager = "less -FR";
-      theme = "RosePine";
+      # pager = "less -FR";
+      theme = "Rose Pine";
     };
   };
 }
