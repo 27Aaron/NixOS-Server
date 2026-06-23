a777b06bac265b5bbf58301827d6144e2076eb7a
Aaron <85681241+27Aaron@users.noreply.github.com>
2026-06-23 16:27:32 +0800

    desktop: add niri user packages and persistence

--- modules/desktop/niri/default.nix
+++ modules/desktop/niri/default.nix
@@ -1,4 +1,12 @@
-{
+{pkgs, ...}: {
+  hm'.home.packages = with pkgs; [
+    firefox
+    fuzzel
+    xwayland-satellite
+  ];
+
+  preservation'.user.directories = [".config/niri"];
+
   programs.niri = {
     enable = true;
   };
