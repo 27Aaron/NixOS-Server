e4daa61fc46e91c9b8701b96c4b888f2b2b69342
Aaron <85681241+27Aaron@users.noreply.github.com>
2026-06-23 14:16:40 +0800

    feat(beelink): 启用 fail2ban 并规整 services' 配置
    将分散的 services'.<svc>.enable 聚合为单个 attrset，并新增 fail2ban.enable。

--- hosts/beelink/default.nix
+++ hosts/beelink/default.nix
@@ -1,12 +1,14 @@
 {
   imports = [
-    # Include the results of the hardware scan.
     ./hardware.nix
   ];
 
-  networking.networkmanager.enable = true;
+  services' = {
+    vnstat.enable = true;
+    openssh.enable = true;
+    fail2ban.enable = true;
+    networkmanager.enable = true;
+  };
 
-  services'.vnstat.enable = true;
-  services'.openssh.enable = true;
   security'.firewall.enable = true;
 }
