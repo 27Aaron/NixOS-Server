e4322401ea516b1bed600a4c13e5e85511c56bb7
Aaron <85681241+27Aaron@users.noreply.github.com>
2026-06-23 14:51:11 +0800

    desktop: add fcitx5 rime input method

---
+++ modules/desktop/fcitx5/default.nix
@@ -0,0 +1,15 @@
+{pkgs, ...}: {
+  i18n = {
+    inputMethod = {
+      type = "fcitx5";
+      enable = true;
+      fcitx5 = {
+        waylandFrontend = true;
+        addons = with pkgs; [
+          fcitx5-rime
+          (qt6Packages.fcitx5-configtool.override {kcmSupport = false;})
+        ];
+      };
+    };
+  };
+}
